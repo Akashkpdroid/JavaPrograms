85d41c0e12d4e4bf35ddf124b5f6bfe9168e45d8
Akashkpdroid <71496133+Akashkpdroid@users.noreply.github.com>
2020-10-01 20:19:26 +0530

    recursion is used

--- factorial.java
+++ factorial.java
@@ -1,15 +1,12 @@
 import java.util.*;
-public class factorial {
-    public static void main(String args[]){
-        int temp=1;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number for factorial: ");
-        int i=sc.nextInt();
-        for(int k=1; k<=i; k++){
-            temp = temp*k;
-        }
-        System.out.println(temp);
-
-
-}
-}
+class FactorialExample2{    
+ public static void main(String args[]){  
+  Scanner sc=new Scanner(System.in);
+   System.out.println("Enter a number");
+   System.out.println("Factorial of is: "+factorial(sc.nextInt()));    
+ }  
+  static int factorial(int n){    
+  if (n == 0)    return 1;    
+  else    return(n * factorial(n-1));    
+ }  
+}  
